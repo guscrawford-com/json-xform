2b588917d1fcd7b5afb8e954a33b93d96ba15628
Gus Crawford <crawford.gus@gmail.com>
2019-06-26 15:55:05 -0400

    add docs

--- src/templates/templater.ts
+++ src/templates/templater.ts
@@ -7,21 +7,49 @@ import { RemoveOperation } from "../operations/remove-operation";
 
 const AWOL = -1;
 
+/**
+ * Configures behavior of a `Templater`
+ */
 export interface TemplaterConfig {
+    /**
+     * Controls scaffolding options
+     */
     scaffolding:{
+        /**
+         * Controls syntax for scaffolding
+         */
         syntax:{
+            /** The indicator that **scaffolding is open** */
             open:string;
+            /** The indicator that **scaffolding is finished** */
             close:string;
+            /**
+             * Controls syntax for filters
+             */
             filter:{
+                /** The indicator that **filtering is open** */
                 open:string;
+                /** The indicator that **filtering is finished** */
                 close:string;
+                /** The indicator separating **filtering arguments** */
                 delim:string;
             },
+            /**
+             * Controls syntax for referencing data on JSON objects
+             */
             reference:{
+                /** The indicator separating **object-property path resolutions** */
                 delim:string;
             }
         };
     };
+    /**
+     * Contains arrow functions used to transform exressions:
+     * `(args:any[])=>any`
+     * Where:
+     * - args[0] is always the instance of the `Templater`
+     * - args[1..] are the delimeter (, by default) separated filter arguments
+     */
     filters: {
         [key:string]:(args:any[])=>any
     }
